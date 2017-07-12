d8c9b89718d92f4304dda01e876b7ca371e25838
= <carlos.matallana@monetisoft.com>
2017-07-12 09:02:53 -0500

    frist commit

---
+++ app/src/main/java/sky/dark/kenny/darksky/ui/activities/SplashScreen.java
@@ -0,0 +1,15 @@
+package sky.dark.kenny.darksky.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sky.dark.kenny.darksky.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
---
+++ app/src/main/java/sky/dark/kenny/darksky/ui/de.java
@@ -0,0 +1,8 @@
+package sky.dark.kenny.darksky.ui;
+
+/**
+ * Created by desarrollo-carlos on 12/07/17.
+ */
+
+public class de {
+}
